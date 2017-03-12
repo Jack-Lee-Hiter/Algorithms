564bff589f0b3e7b9e1090b3de664b09aaaadb0b
Jack-Lee-Hiter <jack.lee@outlook.com>
2017-03-12 21:07:33 +0800

    add binarysearch method with loop

--- src/BinarySearch/BinarySearch.java
+++ src/BinarySearch/BinarySearch.java
@@ -1,6 +1,6 @@
 
 package BinarySearch;
-import java.util.Arrays;
+
 public class BinarySearch {
 	// 递归实现二分查找
 	public static int rank(int key, int[] a)
@@ -27,11 +27,33 @@ public class BinarySearch {
 			return mid;
 		}
 	}
+	public static int rankWithLoop(int key, int[] a)
+	{
+		int lo = 0;
+		int hi = a.length - 1;
+		while (lo <= hi)
+		{
+			int mid = lo + (hi - lo) / 2;
+			if (key < a[mid])
+			{
+				hi = mid - 1;
+			}
+			else if (key > a[mid])
+			{
+				lo = mid + 1;
+			}
+			else
+			{
+				return mid;
+			}
+		}
+		return -1;
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int[] a = {1, 2, 3, 10, 52, 71, 89};
 		int key = 52;
-		int res = rank(52, a);
+		int res = rank(key, a);
 		System.out.println(res + 1);
 
 	}
