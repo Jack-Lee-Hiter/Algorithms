a64d36fa85ae593a1defe44fc9e5c304e019eede
Jack-Lee-Hiter <jack.lee@outlook.com>
2017-04-08 23:53:25 +0800

    addTwoNumbers

---
+++ src/leetCode/addTwoNumbers.java
@@ -0,0 +1,50 @@
+package leetCode;
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+
+public class addTwoNumbers {
+	public static ListNode addTwoNums(ListNode l1, ListNode l2)
+	{
+		ListNode prev = new ListNode(0);
+        ListNode head = prev;
+        int carry = 0;
+        
+        while((l1 != null) || (l2 != null) || (carry != 0))
+        {
+            ListNode cur = new ListNode(0);
+            int sum = ((l1 != null) ? l1.val : 0) + ((l2 != null) ? l2.val : 0) + carry;
+            cur.val = sum % 10;
+            carry = sum / 10;
+        
+            prev.next = cur;
+            prev = cur;
+            
+            l1 = (l1 != null) ? l1.next : l1;
+            l2 = (l2 != null) ? l2.next : l2;
+        }
+        return head.next;
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ListNode l1 = new ListNode(2);
+		l1.next = new ListNode(4);
+		l1.next.next = new ListNode(3);
+		ListNode l2 = new ListNode(5);
+		l2.next = new ListNode(6);
+		l2.next.next = new ListNode(4);
+		ListNode res = addTwoNums(l1, l2);
+		while(res != null)
+		{
+			System.out.println(res.val);
+			res = res.next;
+		}
+	}
+
+}
